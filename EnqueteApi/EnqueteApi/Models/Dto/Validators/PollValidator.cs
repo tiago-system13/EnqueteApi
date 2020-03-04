1b8d6778b46c44b1a809bc67fdf232a965943f98
tiago-system13 <tiago.infor13@gmail.com>
2020-03-03 21:10:16 -0300

    Ajustando validador do Dto Poll

--- EnqueteApi/EnqueteApi/Models/Dto/Validators/PollValidator.cs
+++ EnqueteApi/EnqueteApi/Models/Dto/Validators/PollValidator.cs
@@ -11,10 +11,12 @@ namespace EnqueteApi.Models.Dto.Validators
 
             RuleFor(e => e.PollDescription).Length(5,40).WithMessage("O campo pollDescription deve conter no mínimo 5 caracteres e no máximo 40 caractes");
 
+            RuleFor(e => e.Options).NotEmpty().WithMessage("Preencha pelo menos uma opção de voto");
+
             RuleForEach(x => x.Options).NotEmpty()
                                       .Must((option) => !string.IsNullOrWhiteSpace(option.OptionDescription))
                                       .WithMessage(@"O preenchimento do campo OptionDescription é obrigatório")
-                                      .Must((option) => option.OptionDescription.Length < 40)
+                                      .Must((option) => !string.IsNullOrWhiteSpace(option.OptionDescription) && option.OptionDescription.Length < 40)
                                       .WithMessage(@"O preenchimento do campo OptionDescription deve conter no máximo 40 caracteres");
 
         }
